9d79e1597e8781da8de91ceebfea291884d82c43
Sebastien Hernoux <sebastien.hernoux@ocitocine.com>
2021-03-31 12:58:15 +0200

    Add port log

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(parseInt(process.env.PORT) | 3000);
+  console.log(await app.getUrl());
+  console.log('port : ', parseInt(process.env.PORT) | 3000);
 }
 bootstrap();
