16845243fd9783ca646eb55895860166a44d4227
Sebastien Hernoux <sebastien.hernoux@ocitocine.com>
2021-04-01 10:13:26 +0200

    Remove logs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(parseInt(process.env.PORT) | 3000);
-  console.log(await app.getUrl());
-  console.log('port : ', parseInt(process.env.PORT) | 3000);
 }
 bootstrap();
