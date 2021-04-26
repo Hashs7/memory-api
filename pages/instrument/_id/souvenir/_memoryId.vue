be5faaa9328b6cf1548f1805b5331495087572a6
Hashs7 <seb_her@hotmail.fr>
2021-04-26 16:09:14 +0200

    Show nested route with blur

--- pages/instrument/_id/souvenir/_memoryId.vue
+++ pages/instrument/_id/souvenir/_memoryId.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="memory">
-    <h1 v-if="memory">{{ memory.name }}</h1>
+    <div class="memory__container">
+      <h1 v-if="memory" class="memory__title">{{ memory.name }}</h1>
+    </div>
+    <nuxt-link :to="closeMemory" class="memory__background"></nuxt-link>
   </div>
 </template>
 
@@ -17,6 +20,43 @@ export default {
       const { memoryId } = this.$route.params;
       return this.instrument?.memories.find((m) => m._id === memoryId);
     },
+    closeMemory() {
+      const { id } = this.$route.params;
+      return `/instrument/${id}`;
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.memory {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 40px;
+}
+
+.memory__background {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  backdrop-filter: blur(5px);
+  height: 100vh;
+}
+
+.memory__container {
+  position: relative;
+  z-index: 10;
+  padding: 32px;
+  background-color: #fff;
+}
+
+.memory__title {
+  font-size: 40px;
+}
+</style>
