a38dcdf494ee194a97604e64c5c1a6f9493edbd7
Hashs7 <seb_her@hotmail.fr>
2021-04-29 17:45:08 +0200

    Template survey questions

--- components/instrument/Survey.vue
+++ components/instrument/Survey.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="o-page--full survey">
-    <div class="survey__indicator"></div>
+  <div :class="[questions[index].template]" class="o-page--full survey">
+    <div class="survey__indicator">
+      <span
+        v-for="(q, i) in questions"
+        :key="q"
+        :class="{ active: index === i }"
+        class="survey__indicator-item"
+      ></span>
+    </div>
+    <p class="survey__title">
+      {{ questions.length }} questions pour un champion
+    </p>
     <div class="survey__container">
       <div class="question">
-        <span class="question__title">{{ questions[index].title }}</span>
+        <p class="question__title">{{ questions[index].title }}</p>
+        <label class="question__response">
+          <textarea
+            v-model="questions[index].response"
+            placeholder="Répondre"
+          ></textarea>
+        </label>
       </div>
     </div>
     <div class="survey__controls">
@@ -26,17 +42,32 @@ export default {
         {
           title: 'Dernier voyage avec ? ✈',
           response: '',
-          template: '',
+          template: 'black',
         },
         {
           title: 'Ce petit “je ne sais quoi” qui vous a fait craquer ? 😏',
           response: '',
-          template: '',
+          template: 'pink',
         },
         {
           title: 'Une chanson préférée à jouer avec ? 🎶',
           response: '',
-          template: '',
+          template: 'blue-light',
+        },
+        {
+          title: 'Une chanson préférée à jouer avec ? 🎶',
+          response: '',
+          template: 'blue-dark',
+        },
+        {
+          title: 'Une chanson préférée à jouer avec ? 🎶',
+          response: '',
+          template: 'yellow',
+        },
+        {
+          title: 'Une chanson préférée à jouer avec ? 🎶',
+          response: '',
+          template: 'brown',
         },
       ],
     };
@@ -57,7 +88,74 @@ export default {
 
 <style lang="scss" scoped>
 .survey {
-  margin-top: 100px;
+  padding-top: 100px;
+  text-align: center;
+}
+
+.survey__title {
+  margin-top: 32px;
+  font-size: 15px;
+  font-weight: 700;
+}
+
+.survey__indicator {
+  display: flex;
+  justify-content: center;
+}
+
+.survey__indicator-item {
+  max-width: 48px;
+  width: 100%;
+  height: 4px;
   background-color: $white;
+  opacity: 0.5;
+  margin: 0 2px;
+  border-radius: 4px;
+
+  &.active {
+    opacity: 1;
+  }
+}
+
+.question__title {
+  margin-top: 58px;
+  font-size: 24px;
+}
+
+.question__response {
+  display: block;
+  margin: 52px 28px;
+}
+
+.survey {
+  &.black {
+    color: white;
+    background-color: black;
+  }
+
+  &.pink {
+    color: white;
+    background-color: pink;
+  }
+
+  &.blue-light {
+    color: white;
+    background-color: #68beed;
+  }
+
+  &.blue-dark {
+    color: white;
+    background-color: #3764e5;
+  }
+
+  &.yellow {
+    color: white;
+    background-color: #fbc250;
+  }
+
+  &.brown {
+    color: #dd2303;
+    background-color: #fbf4df;
+  }
 }
 </style>
