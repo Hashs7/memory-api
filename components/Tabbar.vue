0eada29094dde4a4814528dfc729985c073c98e7
Robin Simonklein <rsimonklein@gmail.com>
2021-05-04 14:35:53 +0200

    Add mobile tabbar

---
+++ components/Tabbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav class="tabbar">
+    <nuxt-link
+      v-for="navItem in nav"
+      :key="navItem.path"
+      :to="navItem.path"
+      class="tabbar__item"
+      :class="{ 'tabbar__item--current': $route.path === navItem.path }"
+    >
+      <span class="tabbar__icon"></span>
+      <span class="tabbar__text">{{ navItem.label }}</span>
+    </nuxt-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Tabbar',
+  data() {
+    return {
+      nav: [
+        {
+          label: 'Instruments',
+          path: '/instrument',
+        },
+        {
+          label: 'Profil',
+          path: '/profil',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.tabbar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-around;
+  padding: 0.5rem 0 env(safe-area-inset-bottom, 0) 0; // Avoid iOS notch
+  background-color: white;
+  box-shadow: 0px -0.3rem 0.5rem 0 rgba(100, 100, 100, 0.05);
+
+  &__item {
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    &--current {
+      .tabbar__icon {
+        background-color: gray;
+      }
+    }
+  }
+
+  &__icon {
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 100%;
+    background-color: lightgray;
+  }
+
+  &__text {
+    font-size: 0.6rem;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <Header />
-
     <section class="main-content">
       <nuxt />
     </section>
+    <Tabbar />
   </div>
 </template>
 
 <script>
 import Header from '@/components/layout/Header';
+import Tabbar from '../components/Tabbar';
 
 export default {
   components: {
+    Tabbar,
     Header,
   },
 };
