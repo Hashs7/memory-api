4e7209bd93f5f9222a9d218179a32bac28830d69
Hashs7 <seb_her@hotmail.fr>
2021-06-01 16:25:19 +0200

    Update file

--- src/file/file.helper.ts
+++ src/file/file.helper.ts
@@ -1,5 +1,6 @@
 import { File } from './file.schema';
 
 export function rewritePath(file: File): string {
+  if (file.path.startsWith(process.env.API_BASE_URL)) return file.path;
   return `${process.env.API_BASE_URL}/file/${file.path}`;
 }
