9553db7c84eb43232aa41f474a8ed69ff3d80034
ClementDums <dumas.clt@gmail.com>
2021-06-07 11:32:52 +0200

    User search

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -3,19 +3,22 @@ import { InstrumentService } from '../instrument/instrument.service';
 import { MemoryService } from '../instrument/memory/memory.service';
 import { Instrument } from '../instrument/instrument.schema';
 import { Types } from 'mongoose';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user.schema';
 
 @Injectable()
 export class SearchService {
   constructor(
     private instrumentService: InstrumentService,
     private memoryService: MemoryService,
+    private userService: UserService,
   ) {}
   public async search(
     text: string,
     categories?: Types.ObjectId[],
     forSale?: string,
     instruTypes?: string,
-  ): Promise<{ instruments: Instrument[]; memories: Object[] }> {
+  ): Promise<{ instruments: Instrument[]; memories: Object[]; users: User[] }> {
     let instrumentRes = await this.instrumentService.search(
       text,
       forSale,
@@ -25,9 +28,12 @@ export class SearchService {
 
     const memoryRes = await this.memoryService.search(text, categories);
 
+    const userRes = await this.userService.search(text);
+
     return {
       instruments: instrumentRes,
       memories: memoryRes,
+      users: userRes,
     };
   }
 }
