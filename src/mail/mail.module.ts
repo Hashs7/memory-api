7ad4624b433d4b569d0c96738184b1be7242fb6d
Hashs7 <seb_her@hotmail.fr>
2021-06-07 22:43:25 +0200

    update mail

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,40 +1,40 @@
-import {MailerModule} from '@nestjs-modules/mailer';
-import {HandlebarsAdapter} from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import {Module} from '@nestjs/common';
-import {MailService} from './mail.service';
-import {join} from 'path';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
 // or
 @Module({
-    imports: [
-        MailerModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                transport: {
-                    host: configService.get('MAIL_HOST'),
-                    secure: false,
-                    auth: {
-                        user: configService.get('MAIL_USER'),
-                        pass: configService.get('MAIL_PASSWORD'),
-                    },
-                },
-                defaults: {
-                    from: `"No Reply" <${configService.get('MAIL_FROM')}>`,
-                },
-                template: {
-                    dir: join(__dirname, 'templates'),
-                    adapter: new HandlebarsAdapter(),
-                    options: {
-                        strict: true,
-                    },
-                },
-            })
-        }),
-    ],
-    providers: [MailService],
-    exports: [MailService],
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: configService.get('MAIL_USER'),
+            pass: configService.get('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${configService.get('MAIL_FROM')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
