acfaaba4486bbb6dc95a4ffa0b861f64e50f3df4
Hashs7 <seb_her@hotmail.fr>
2021-06-07 11:27:24 +0200

    Add gzip compression on requests

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(compression());
 
   const config = new DocumentBuilder()
-      .setTitle('Memory API')
-      .setVersion('1.0')
-      .build();
+    .setTitle('Memory API')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
